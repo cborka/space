03e7b2a8ff1d199e21d08a2abcbdd3fa433567c2
Boris Ts. (cborka) <cborka777@yandex.ru>
2019-11-15 23:38:55 +0700

    2019-15-11 Установил nodemon

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ var pgp = require("pg-promise")(/*options*/);
 var db = pgp(process.env.PG_CONNECT);
 
 console.log("Подключена база данных: "+db);
-///console.log("Режим запуска: "+process.env.NODE_ENV);
+//console.log("Режим запуска: "+process.env.NODE_ENV);
 
 module.exports = db;
